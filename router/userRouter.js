848e180d31e9e1e45fe54c6f514f4ac5b44b572e
Berni <pbernal@mendoza.edu.ar>
2024-12-10 21:53:05 -0300

    app completa para deploy

--- router/userRouter.js
+++ router/userRouter.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
 // librería para encriptar contraseñas
-const bcrypt = require('bcrypt');
+const bcryptjs = require('bcryptjs');
 
 //importamos el esquema del usuario
 const User = require('../models/userModel');
@@ -101,10 +101,10 @@ router.post("/register",
             console.log(usuarioExiste);
 
             
-            const salt = bcrypt.genSaltSync(10);
-            console.log(salt);
+            //const salt = bcrypt.genSaltSync(10);
+            //console.log(salt);
 
-            //persona.password = await bcrypt.hashSync(password, salt);
+            persona.password = await bcryptjs.hash(password, 10);
             console.log(persona.password);
 
             const newUser = new User(persona);
